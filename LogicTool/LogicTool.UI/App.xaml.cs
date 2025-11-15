b6a8d92890e40f664bfee48ac2373aa3c00a6a24
KockAnigilator <jonikm892@gmail.com>
2025-11-15 08:37:51 +0300

    VIew

---
+++ LogicTool/LogicTool.UI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using LogicTool.Business.Interfaces;
+using LogicTool.Business.Services;
+using System.Windows;
+
+namespace LogicTool.UI
+{
+    public partial class App : Application
+    {
+        
+    }
+}
